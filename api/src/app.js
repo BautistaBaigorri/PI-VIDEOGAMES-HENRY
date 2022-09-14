ff916a45ea6f5c05360f647c83657cd8e4b955d4
Bautista Baigorri <bautibaigorri.bb@gmail.com>
2022-09-14 16:19:00 -0300

    fix app

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
@@ -10,9 +11,9 @@ const cors = require("cors");
 
 server.name = "API";
 
-server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(cors());
-server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
@@ -22,7 +23,6 @@ server.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
